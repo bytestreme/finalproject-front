bbb4c847b156ffcac3db76deac49db657fed95b6
nurper <nurken_99@mail.ru>
2019-11-19 17:46:11 +0600

    Logs functionality, agent page ticket

--- src/components/logs/Logs.vue
+++ src/components/logs/Logs.vue
@@ -1,6 +1,15 @@
 <template>
     <div class="templatemo-content-container">
         <div class="templatemo-content-widget  white-bg">
+            <h2 v-if="$store.getters.role === 'ADMIN'" class="margin-bottom-10">Logs management</h2>
+            <div v-if="$store.getters.role === 'ADMIN'" class="row form-group">
+                <div class="col-lg-6 col-md-6 form-group text-right">
+                    <button style="width: 100%" @click="logSwitch" class="templatemo-blue-button">{{logStatusAction}}</button>
+                </div>
+                <div class="col-lg-6 col-md-6 form-group text-right">
+                    <button style="width: 100%" @click="logType" class="templatemo-blue-button">{{logTypeAction}}</button>
+                </div>
+            </div>
             <h2 class="margin-bottom-10">Logs</h2>
             <div class="templatemo-content-container">
                 <div class="templatemo-content-widget no-padding">
@@ -36,7 +45,37 @@
                 logs: []
             }
         },
+        computed:{
+            logTypeAction(){
+                return this.$store.getters.isLogsDetailed ? "Less Detailed" : "More Detail"
+            },
+            logStatusAction(){
+                return this.$store.getters.logStatus ? "Turn off logs" : "Turn on logs"
+            }
+        },
         methods:{
+            //Logs on or off
+            logSwitch(){
+                axiosInstance.post('/api/admin/switchLog').then(res => {
+                    // eslint-disable-next-line no-console
+                    console.log("OK: " + res.data);//true or false
+                    this.$store.dispatch('logStatusChange', res.data).then(this.getLogs());
+                }).catch(error => {
+                    // eslint-disable-next-line no-console
+                    console.log(error)
+                });
+            },
+            //Logs detailed or not change
+            logType(){
+                axiosInstance.post('/api/admin/logTypeChange').then(res => {
+                    // eslint-disable-next-line no-console
+                    console.log("OK: " + res.data);
+                    this.$store.dispatch('logTypeChange', res.data).then(this.getLogs()); //true or false :isDetailed
+                }).catch(error => {
+                    // eslint-disable-next-line no-console
+                    console.log(error)
+                });
+            },
             getLogs(){
                 this.logs = [];
                 axiosInstance.get(
@@ -50,7 +89,7 @@
                     if (res.status === 200) {
                         // eslint-disable-next-line no-console
                         console.log("OK: " + res.data);
-                        this.logs = res.data; //stations is array
+                        this.logs = res.data;
                     } else {
                         // eslint-disable-next-line no-console
                         console.log("BAD: " + res.status);
