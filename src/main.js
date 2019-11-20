7ea203ee6a1c6b115456bfe01bfba8b838b4ceec
Shynggys Sapraliyev <Shynggys_Sapraliyev@epam.com>
2019-11-21 02:20:21 +0600

    resolved

--- src/main.js
+++ src/main.js
@@ -4,7 +4,6 @@ import store from "./store";
 import VueRouter from 'vue-router'
 import VueMask from 'v-mask'
 import * as VueGoogleMaps from "vue2-google-maps";
-
 import DashboardPage from "./components/dashboard/DashboardPage";
 import LoginPage from "./components/login/LoginPage";
 import RegisterPage from "./components/register/RegisterPage";
@@ -30,13 +29,14 @@ Vue.config.productionTip = false;
 Vue.use(Notifications, {velocity})
 Vue.use(VueRouter);
 Vue.use(VueMask);
+
+
 Vue.use(VueGoogleMaps, {
     load: {
         key: "AIzaSyD8zIuo7-zcpOs6EaQmhlu58y8qT9_qp9Y",
         libraries: "places" // necessary for places input
     }
 });
-
 
 const routes = [
     {
